63e3f9e646c8d5cb095dbecdf84239ad683f6fe5
howard-chang24 <71419698+howard-chang24@users.noreply.github.com>
2020-12-17 12:16:59 +0800

    week14
    a109260045

---
+++ start.py
@@ -0,0 +1,13 @@
+import turtle
+shelly=turtle.Turtle()
+shelly.shape("turtle")
+shelly.position()
+shelly.color("purple","blue")
+shelly.pencolor("yellow")
+shelly.turtlesize(10,10,2)
+shelly.resizemode("255")
+shelly.turtlesize(5,5,3)
+import turtle
+for i in range(5):
+   shelly.forward(100)
+   shelly.right(144)
